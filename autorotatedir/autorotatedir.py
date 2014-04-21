0acb151522f918fab65ab73c574731cb5f7ce167
krets <jesse@krets.com>
2014-04-20 17:57:28 -0700

    Rotator class setup; adding logging
    Building a basic class to carry the necessary parameters and setup a
    logger for the verbosity level.

--- autorotatedir/autorotatedir.py
+++ autorotatedir/autorotatedir.py
@@ -2,11 +2,13 @@ import os
 import errno
 import sys
 import datetime
+import logging
 import argparse
 
 OUTPUT_DIR = ".old"
 ROTATION_DAYS = 30
 EXPIRATION_DAYS = 0
+DATE_FORMAT = "%Y%m"
 
 def mkdirs(newdir, mode=0777):
     try: os.makedirs(newdir, mode)
@@ -14,6 +16,44 @@ def mkdirs(newdir, mode=0777):
         # Reraise the error unless it's about an already existing directory 
         if err.errno != errno.EEXIST or not os.path.isdir(newdir): 
             raise
+
+class Rotator(object):
+    """ Rotator for handling the walking, moving and deleting of files in a 
+        given directory.
+    """
+    def __init__(self, directory, age=ROTATION_DAYS, expiration=EXPIRATION_DAYS,
+                 outputdir=OUTPUT_DIR, verbose=False, dateformat=DATE_FORMAT):
+        """
+        :param directory: Directory to act on.
+        :param age: Age of files to rotate.
+        :param expiration: Age of files to delete.
+        :param outputdir: Destination directory for rotated files.
+        :param verbose: Flag whether to show verbose output
+        :type verbose: boolean
+        """
+        self.dir = directory
+        self.age = age
+        self.exp = expiration
+        self.out = outputdir
+        self.date_format = dateformat
+        self.log = logging.getLogger('autorotatedir')
+
+        # Prepare the logger
+        log_handler = logging.StreamHandler()
+        log_formatter = logging.Formatter("%(levelname)s: %(message)s")
+        log_handler.setFormatter(log_formatter)
+        self.log.addHandler(log_handler)
+
+        self.setVerbose(verbose)
+
+    def process(self):
+        self.log.info('Processing: %s' % self.dir)
+        pass
+
+    def setVerbose(self, verbose):
+        level = logging.WARNING
+        if verbose: level = logging.INFO
+        self.log.setLevel(level)
 
 def main():
     desc = 'Download Directory Rotator - This tool will help cleanup desktop '\
@@ -45,6 +85,8 @@ def main():
                         default=EXPIRATION_DAYS)
 
     args = vars(parser.parse_args(sys.argv[1:]))
+    rot = Rotator(**args)
+    rot.process()
     pass
 
 if __name__ == '__main__':
