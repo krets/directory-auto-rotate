447df9b03b6c6a6aa437f9ae7ca5ee595946d24b
krets <jesse@krets.com>
2014-04-20 19:22:51 -0700

    most basic functionality implemented (v0.1)
    Implemented most of the basic functionality. Still need to setup file
    expiration for old files.

--- autorotatedir/autorotatedir.py
+++ autorotatedir/autorotatedir.py
@@ -1,28 +1,26 @@
+__author__ = 'Jesse Kretschmer'
+__version__ = '0.1'
 import os
 import errno
 import sys
-import datetime
+from datetime import datetime, timedelta
 import logging
 import argparse
+import time
+import shutil
 
 OUTPUT_DIR = ".old"
 ROTATION_DAYS = 30
 EXPIRATION_DAYS = 0
 DATE_FORMAT = "%Y%m"
 
-def mkdirs(newdir, mode=0777):
-    try: os.makedirs(newdir, mode)
-    except OSError, err:
-        # Reraise the error unless it's about an already existing directory 
-        if err.errno != errno.EEXIST or not os.path.isdir(newdir): 
-            raise
-
 class Rotator(object):
     """ Rotator for handling the walking, moving and deleting of files in a 
         given directory.
     """
     def __init__(self, directory, age=ROTATION_DAYS, expiration=EXPIRATION_DAYS,
-                 outputdir=OUTPUT_DIR, verbose=False, dateformat=DATE_FORMAT):
+                 outputdir=OUTPUT_DIR, verbose=False, dateformat=DATE_FORMAT,
+                 keepempty=False):
         """
         :param directory: Directory to act on.
         :param age: Age of files to rotate.
@@ -31,12 +29,14 @@ class Rotator(object):
         :param verbose: Flag whether to show verbose output
         :type verbose: boolean
         """
-        self.dir = directory
-        self.age = age
-        self.exp = expiration
+        self.dir = os.path.abspath(directory)
+        self.old_diff = timedelta(days=age)
+        self.exp_diff = timedelta(days=expiration)
         self.out = outputdir
+        self.keepempty = keepempty
         self.date_format = dateformat
         self.log = logging.getLogger('autorotatedir')
+        self.now = datetime.now()
 
         # Prepare the logger
         log_handler = logging.StreamHandler()
@@ -47,8 +47,41 @@ class Rotator(object):
         self.setVerbose(verbose)
 
     def process(self):
-        self.log.info('Processing: %s' % self.dir)
-        pass
+        # self.dir must be an absolute path
+        destination = os.path.join(self.dir, self.out)
+        empty_dirs = []
+        for path, dirs, files in os.walk(self.dir):
+            # Try to skip over the output directories
+            compare_dirs = [path + os.path.sep, 
+                            destination + os.path.sep]
+            if os.path.commonprefix(compare_dirs) == destination:
+                self.log.info("Ignoring output dir: %s" % path)
+                continue
+            self.log.info("Processing: %s" % path)
+            for f in files:
+                src_file = os.path.join(path, f)
+                modified = datetime.fromtimestamp(os.path.getmtime(src_file))
+                if modified + self.old_diff < self.now:
+                    date_dir = modified.strftime(self.date_format)
+                    sub_dir = path.replace(self.dir, '').lstrip(os.path.sep)
+                    dst_dir = os.path.join(destination, date_dir, sub_dir)
+                    if not os.path.isdir(dst_dir):
+                        self.log.info("Creating dir: %s" % dst_dir)
+                        try:
+                            os.makedirs(dst_dir)
+                        except Exception, e:
+                            msg = "Can't make destination, %s: %s" % (dst_dir,e)
+                            self.log.error(msg)
+                            continue
+                    self.log.info("Rotating file: %s to %s"%(src_file,dst_dir))
+                    shutil.move(src_file, dst_dir)
+
+            if not os.listdir(path):
+                empty_dirs.insert(0, path)
+
+        if not self.keepempty:
+            for d in empty_dirs:
+                os.rmdir(d)
 
     def setVerbose(self, verbose):
         level = logging.WARNING
@@ -67,6 +100,9 @@ def main():
                              'Default: (%s)' % OUTPUT_DIR, 
                         default=OUTPUT_DIR)
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-k', '--keepempty', action='store_true',
+                        help='Keep empty directories. Empty directories will '\
+                             'otherwise be removed.')
     parser.add_argument('-a', 
                         '--age', 
                         help='Age of files to rotate. Any file or folders '\
@@ -87,7 +123,6 @@ def main():
     args = vars(parser.parse_args(sys.argv[1:]))
     rot = Rotator(**args)
     rot.process()
-    pass
 
 if __name__ == '__main__':
     main()
