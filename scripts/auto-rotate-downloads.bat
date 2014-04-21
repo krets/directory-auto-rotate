b854b573dd29c4a260dc8e972b77d6dbe13691c4
krets <jesse@krets.com>
2014-04-20 21:55:19 -0700

    Minimize the batch execution
    This is not perfect, but the batch script should run twice, and the
    second time it will be minimized.

--- scripts/auto-rotate-downloads.bat
+++ scripts/auto-rotate-downloads.bat
@@ -1,4 +1,11 @@
 @echo off
+rem http://danrice.net/articles/run-a-batch-script-minimized
+if not "%minimized%"=="" goto :minimized
+set minimized=true
+start /min cmd /C "%~dpnx0"
+goto :EOF
+:minimized
+rem Anything after here will run in a minimized window
 cd /d %~dp0
 rem Run auto rotate on the current user download directory (windows 7 default)
 rem Also the -e flag will delete files older than a year.
